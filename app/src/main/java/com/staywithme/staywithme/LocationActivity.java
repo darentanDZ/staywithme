0852e7c570f1fbdf4625750c2259ca87ef5b27db
Gerald <szeinn94@gmail.com>
2016-12-11 00:38:45 +0800

    Gerald Commit V2

---
+++ app/src/main/java/com/staywithme/staywithme/LocationActivity.java
@@ -0,0 +1,44 @@
+package com.staywithme.staywithme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import belka.us.androidtoggleswitch.widgets.ToggleSwitch;
+
+public class LocationActivity extends AppCompatActivity {
+
+    private ToggleSwitch switchLocation;
+    private TextView addressText;
+    private Button nextButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+        switchLocation = (ToggleSwitch) findViewById(R.id.locationSwitch);
+        addressText = (TextView) findViewById(R.id.locationText);
+
+        switchLocation.setOnToggleSwitchChangeListener(new ToggleSwitch.OnToggleSwitchChangeListener(){
+
+            @Override
+            public void onToggleSwitchChangeListener(int position, boolean isChecked) {
+                // Write your code ...
+                switch(position) {
+                    case 0:
+                        addressText.setVisibility(View.VISIBLE);
+                        break;
+                    case 1:
+                        addressText.setVisibility(View.GONE);
+                        addressText.setText("");
+                        break;
+                }
+            }
+        });
+    }
+}
