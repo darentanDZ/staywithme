383cc4ea04ce07c257577f5537a3797b35becf9a
whyteaz <elfwhytea@gmail.com>
2016-12-10 16:56:55 +0800

    Added ViewMessageActivity by Tien

---
+++ app/src/main/java/com/staywithme/staywithme/ViewMessageActivity.java
@@ -0,0 +1,13 @@
+package com.staywithme.staywithme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_message);
+    }
+}
