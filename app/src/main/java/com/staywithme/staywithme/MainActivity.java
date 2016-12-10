4edf45d599a15de561b3abf70662d81ec4fdf156
whyteaz <elfwhytea@gmail.com>
2016-12-10 15:56:10 +0800

    Tien Commit

--- app/src/main/java/com/staywithme/staywithme/MainActivity.java
+++ app/src/main/java/com/staywithme/staywithme/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //HELLO THIS IS A TESTING FOR GITHUB
+        //FROM TIEN
     }
 }
