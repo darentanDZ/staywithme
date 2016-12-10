c4d0fdaf53ec6df34e22062a27bb02a2cf1291f5
whyteaz <elfwhytea@gmail.com>
2016-12-10 20:06:35 +0800

    Style View Message Layout

---
+++ app/src/main/java/com/staywithme/staywithme/VideoActivity.java
@@ -0,0 +1,32 @@
+package com.staywithme.staywithme;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        View decorView = getWindow().getDecorView();
+// Hide both the navigation bar and the status bar.
+// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+// a general rule, you should design your app to hide the status bar whenever you
+// hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        VideoView mVideoView = (VideoView) findViewById(R.id.video_view);
+        mVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() +"/"+R.raw.testvideo));
+        mVideoView.setMediaController(new MediaController(this));
+        mVideoView.requestFocus();
+        mVideoView.start();
+    }
+}
--- app/src/main/java/com/staywithme/staywithme/ViewMessageActivity.java
+++ app/src/main/java/com/staywithme/staywithme/ViewMessageActivity.java
@@ -1,13 +1,36 @@
 package com.staywithme.staywithme;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.IOException;
 
 public class ViewMessageActivity extends AppCompatActivity {
 
+    private String PATH_TO_FILE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_message);
+    }
+
+    public void viewMessage(View view) {
+        /*MediaPlayer mp = new MediaPlayer();
+        try {
+            mp.setDataSource(R.raw.testvideo);
+            mp.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mp.start();*/
+
+        /*MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.testvideo );
+        mediaPlayer.start();*/
+
+        Intent intent = new Intent(this, VideoActivity.class);
+        startActivity(intent);
     }
 }
